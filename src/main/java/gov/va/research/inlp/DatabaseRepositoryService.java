78ec574c464d75c951bc4136e9a47e6dbca1b66f
Ryan Cornia <ryan.cornia@utah.edu>
2010-12-09 15:56:56 +0000

    Bug fix build
    git-svn-id: http://10.5.17.158/svn/vinci-nlp/v3nlp-server/trunk@612 0c291781-be06-44b6-a50b-f591c0ec80da

---
+++ src/main/java/gov/va/research/inlp/DatabaseRepositoryService.java
@@ -0,0 +1,15 @@
+package gov.va.research.inlp;
+
+import gov.va.research.inlp.model.datasources.DataServiceSource;
+import gov.va.vinci.cm.DocumentInterface;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DatabaseRepositoryService {
+
+	public abstract List<String> getRespostoryNames();
+
+	public abstract List<DocumentInterface> getDocuments(DataServiceSource m) throws SQLException;
+
+}
