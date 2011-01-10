df65aa00ffee2768e3ceda1d9c86a47ae7b9e880
Ryan Cornia <ryan.cornia@utah.edu>
2011-01-10 18:33:56 +0000

    Bug Fixes
    git-svn-id: http://10.5.17.158/svn/vinci-nlp/v3nlp-server/trunk@852 0c291781-be06-44b6-a50b-f591c0ec80da

--- src/test/java/gov/va/research/inlp/gate/SimpleOperationsTest.java
+++ src/test/java/gov/va/research/inlp/gate/SimpleOperationsTest.java
@@ -1,5 +1,7 @@
 package gov.va.research.inlp.gate;
 
+import org.apache.commons.validator.GenericValidator;
+
 import gov.va.research.inlp.gate.HeaderVO;
 import junit.framework.TestCase;
 
@@ -10,6 +12,15 @@ public class SimpleOperationsTest extends TestCase {
 		HeaderVO c = new HeaderVO();
 		junit.framework.Assert.assertNotNull("Null object.", c);
 		
+		
+		String a = "";
+		if (a != null && a.length() > 0) {
+			
+		}
+		
+		if (GenericValidator.isBlankOrNull(a)) {
+			
+		}
 	}
 
 }
