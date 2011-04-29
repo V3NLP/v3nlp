705648ce6203e05d67bbdc2ce258ab7963c525f4
Ryan Cornia <ryan.cornia@utah.edu>
2011-04-29 16:25:12 +0000

    Improving test coverage.
    git-svn-id: http://10.5.17.158/svn/vinci-nlp/v3nlp-server/trunk@4222 0c291781-be06-44b6-a50b-f591c0ec80da

--- src/test/java/gov/va/vinci/v3nlp/SimpleSetterGetterTest.java
+++ src/test/java/gov/va/vinci/v3nlp/SimpleSetterGetterTest.java
@@ -2,13 +2,19 @@ package gov.va.vinci.v3nlp;
 
 import gov.va.vinci.test.GetterSetterTester;
 import gov.va.vinci.v3nlp.controller.ExpressionSearchForm;
+import gov.va.vinci.v3nlp.expressionlib.Expression;
 import gov.va.vinci.v3nlp.gate.HeaderVO;
 import gov.va.vinci.v3nlp.model.CorpusSummary;
 import gov.va.vinci.v3nlp.model.DocumentPedigreeCount;
 import gov.va.vinci.v3nlp.model.Template;
+import gov.va.vinci.v3nlp.model.datasources.DataServiceSource;
 import gov.va.vinci.v3nlp.model.datasources.TextDocument;
 import gov.va.vinci.v3nlp.model.operations.Concept;
 import gov.va.vinci.v3nlp.model.operations.Sectionizer;
+import gov.va.vinci.v3nlp.registry.NlpComponent;
+import gov.va.vinci.v3nlp.registry.NlpComponentCategory;
+import gov.va.vinci.v3nlp.registry.NlpComponentProvides;
+import gov.va.vinci.v3nlp.registry.NlpComponentRequires;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -19,10 +25,16 @@ public class SimpleSetterGetterTest extends TestCase {
         GetterSetterTester  tester = new GetterSetterTester();
         tester.testClass(Concept.class);
         tester.testClass(CorpusSummary.class);
+        tester.testClass(DataServiceSource.class);
         tester.testClass(DocumentPedigreeCount.class);
+        tester.testClass(Expression.class);
         tester.testClass(ExpressionSearchForm.class);
         tester.testClass(HeaderVO.class);
         tester.testClass(LabelValue.class);
+        tester.testClass(NlpComponent.class);
+        tester.testClass(NlpComponentCategory.class);
+        tester.testClass(NlpComponentProvides.class);
+        tester.testClass(NlpComponentRequires.class);
         tester.testClass(Sectionizer.class);
         tester.testClass(Template.class);
         tester.testClass(TextDocument.class);
