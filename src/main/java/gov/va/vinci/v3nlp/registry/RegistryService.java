f56d4cf23c5b00dbb11c7891aa6c54f272a6425e
Ryan Cornia <ryancornia@Ryan-Cornias-MacBook-Pro.local>
2011-06-08 09:33:41 -0600

    Added javadocs...

--- src/main/java/gov/va/vinci/v3nlp/registry/RegistryService.java
+++ src/main/java/gov/va/vinci/v3nlp/registry/RegistryService.java
@@ -7,8 +7,17 @@ import java.util.List;
 
 public interface RegistryService {
 
+    /**
+     * Get all Nlp Component Categories in the registry
+     * @return  the list of component categories in the system.
+     */
     public List<NlpComponentCategory> getNlpComponentCategoryList();
 
+    /**
+     * Returns a component for a given unique service id.
+     * @param uid the unique service id to get
+     * @return  the nlp component or null if the component was not found.
+     */
     public NlpComponent getNlpComponent(String uid);
 
     /**
