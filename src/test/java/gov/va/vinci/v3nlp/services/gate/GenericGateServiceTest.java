2e62808eac1f653db4f8533eda6615e9ac01b39b
Ryan Cornia <ryan.cornia@utah.edu>
2011-12-01 14:01:37 -0700

    fixed parent

---
+++ src/test/java/gov/va/vinci/v3nlp/services/gate/GenericGateServiceTest.java
@@ -0,0 +1,11 @@
+package gov.va.vinci.v3nlp.services.gate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vhaislcornir
+ * Date: 11/23/11
+ * Time: 8:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericGateServiceTest {
+}
